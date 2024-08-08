26c954c0364f178d938e40c4be59119023472589
Adhiraj Kinlekar <adhirajkkinlekar@gmail.com>
2024-08-08 14:28:44 +0530

    fix mf issues

--- admin_ui/src/app/app-routing.module.ts
+++ admin_ui/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+// import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {
+  //   path: 'academy', 
+  //   loadChildren: () => loadRemoteModule('admin_academy_ui', './AppModule').then(m => m.AppModule)
+  // },
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+ 
